9d41de4355dfc601255a61ba171b54569fdd8cc5
Josue Izaguirre <flynn1411@gmail.com>
2020-09-04 14:07:35 -0600

    v1.4.4 commit

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v1.4.3';
-const dynamicCacheName = 'site-dynamic-v1.4.3';
+const staticCacheName = 'site-static-v1.4.4';
+const dynamicCacheName = 'site-dynamic-v1.4.4';
 
 cacheFiles = [
     './',
